6f522a5d14b07e314d03e92cdb669a0cc0cec7ac
shi <shi@qq.com>
2018-04-19 22:59:53 +0800

    内存--->redis 修改了最大连接数

---
+++ src/hiapp/modules/dm/Test.java
@@ -0,0 +1,10 @@
+package hiapp.modules.dm;
+
+/**
+ * Created by shtou on 2018/4/19.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
